69032440d4e6ce0dc33154f230e088fd487db71b
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-01-27 20:47:03 -0500

    Refactor phone_number_spec with let! block

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,23 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, type: :model do
+  let!(:phone_number) do
+    FactoryBot.create(:phone_number)
+  end
+
   context "phone number contains dashes" do
+    before { phone_number.update!(value: "555-856-8075") }
+
     it "strips out the dashes" do
-      phone_number = FactoryBot.create(:phone_number, value: "555-856-8075")
       expect(phone_number.value).to eq("5558568075")
     end
   end
 
   context "phone number contains parenthese" do
+    before { phone_number.update!(value: "(555) 856-8075") }
+
     it "strips out the parentheses" do
-      phone_number = FactoryBot.create(:phone_number, value: "(555) 856-8075")
       expect(phone_number.value).to eq("5558568075")
     end
   end
 
   context "phone number contains country code" do
+    before { phone_number.update!(value: "+1 555 856 8075") }
+
     it "strips out the country code" do
-      phone_number = FactoryBot.create(:phone_number, value: "+1 555 856 8075")
       expect(phone_number.value).to eq("5558568075")
     end
   end
