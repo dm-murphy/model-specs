b81fd1b99e1c01e0978ce5f15711c4fe60800912
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-01-27 20:42:23 -0500

    Add strip_country_code method and refactor callback

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,7 +1,16 @@
 class PhoneNumber < ApplicationRecord
-  before_validation :strip_non_numeric_from_value
+  EXPECTED_NUMBER_OF_DIGITS = 10
+  before_validation :strip_value
 
-  def strip_non_numeric_from_value
+  def strip_value
+    self.value = strip_country_code(strip_non_numeric)
+  end
+
+  def strip_non_numeric
     self.value = self.value.gsub(/\D/, "")
+  end
+
+  def strip_country_code(value)
+    value.split("").last(EXPECTED_NUMBER_OF_DIGITS).join
   end
 end
